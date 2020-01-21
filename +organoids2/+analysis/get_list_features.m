1b15e2f58a908bb6d771623565ec3ba93a0d3d9e
Lauren Beck <laurenbeck13@gmail.com>
2020-01-21 17:50:26 -0500

    Removed code I was using for analysis. Will keep that on Dropbox.

--- +organoids2/+analysis/get_list_features.m
+++
@@ -1,9 +0,0 @@
-function list_features = get_list_features(data_struct)
-
-    % get all structure fields:
-    list_fields = fieldnames(data_struct);
-    
-    % get all fields containing the name feature:
-    list_features = list_fields(contains(list_fields, 'feature'));
-    
-end
